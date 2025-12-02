e1d13f6ea5af54d8cd2300944630c477f363c61f
Nischal Joshi <nischalzoc999@gmail.com>
2025-12-02 21:39:09 +0545

    feat: task data saving

--- src/components/modal/TaskModal.tsx
+++ src/components/modal/TaskModal.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import type { Task } from "../../types/task";
 
 interface TaskModalProps {
   handleOpen: () => void;
 }
 
 const TaskModal: React.FC<TaskModalProps> = ({ handleOpen }) => {
+  const [task, setTask] = useState({
+    title: "",
+    description: "",
+    priority: "",
+    deadline: "",
+    status: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLFormElement>) => {
+    const { name, value } = e.target;
+    setTask((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  };
+  console.log(task);
+
   const handleModal = () => {
     handleOpen();
   };
@@ -22,6 +42,8 @@ const TaskModal: React.FC<TaskModalProps> = ({ handleOpen }) => {
             type="text"
             placeholder="Task Title"
             className=" w-full border border-green-300  px-7 py-5"
+            onChange={handleChange}
+            name="title"
           />
 
           <textarea
@@ -29,6 +51,8 @@ const TaskModal: React.FC<TaskModalProps> = ({ handleOpen }) => {
             cols={5}
             placeholder="Task Description"
             className=" w-full border border-green-300  px-7 py-5"
+            onChange={handleChange}
+            name="description"
           />
           <div className="w-full flex justify-around">
             <div>
@@ -38,29 +62,50 @@ const TaskModal: React.FC<TaskModalProps> = ({ handleOpen }) => {
                 name="priority"
                 value="Medium"
                 className="text-2xl text-red-600"
+                onChange={handleChange}
               />
               <label htmlFor="medium">Medium</label>
             </div>
 
             <div>
-              <input type="radio" id="high" name="priority" value="High" />
+              <input
+                type="radio"
+                id="high"
+                name="priority"
+                value="High"
+                onChange={handleChange}
+              />
               <label htmlFor="high">High</label>
             </div>
 
             <div>
-              <input type="radio" id="low" name="priority" value="Low" />
+              <input
+                type="radio"
+                id="low"
+                name="priority"
+                value="Low"
+                onChange={handleChange}
+              />
               <label htmlFor="low">Low</label>
             </div>
           </div>
 
           <input
             type="date"
+            name="deadline"
+            onChange={handleChange}
             className=" w-full border border-green-300  px-7 py-5"
           />
 
           <div className="w-full flex justify-around">
             <div>
-              <input type="radio" id="done" name="status" value="Done" />
+              <input
+                type="radio"
+                id="done"
+                name="status"
+                value="Done"
+                onChange={handleChange}
+              />
               <label htmlFor="done">Done</label>
             </div>
 
@@ -70,12 +115,19 @@ const TaskModal: React.FC<TaskModalProps> = ({ handleOpen }) => {
                 id="in-progress"
                 name="status"
                 value="In Progress"
+                onChange={handleChange}
               />
               <label htmlFor="In Progress">In Progress</label>
             </div>
 
             <div>
-              <input type="radio" id="todo" name="status" value="Todo" />
+              <input
+                type="radio"
+                id="todo"
+                name="status"
+                value="Todo"
+                onChange={handleChange}
+              />
               <label htmlFor="todo"> Todo</label>
             </div>
           </div>
