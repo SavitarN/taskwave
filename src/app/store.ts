5cd3f1710a6878155c20a7d212a68f2e0798f5fe
Nischal Joshi <nischalzoc999@gmail.com>
2025-12-09 21:38:26 +0545

    feat: add task reducer to global store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
-import reducer from "../features/user/userSlice";
+import taskReducer from "../features/task/taskSlice";
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    task: taskReducer,
   },
 });
 console.log(store);
--- src/features/task/taskSlice.ts
+++ src/features/task/taskSlice.ts
@@ -2,16 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 import type { Task } from "../../types/task";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
-const initialState: Task = {
-  id: "",
-  title: "",
-  description: "",
-  priority: "medium",
-  deadline: "",
-  status: "in-progress",
-};
+const initialState: Task[] = [];
 const taskSlice = createSlice({
   name: "task",
   initialState,
-  addTask: (state, action: payloadAction<Task>) => {},
+
+  reducers: {
+    addTask: (state, action: PayloadAction<Task>) => {
+      state.push(action.payload);
+    },
+  },
 });
+
+export const { addTask } = taskSlice.actions;
+export default taskSlice.reducer;
